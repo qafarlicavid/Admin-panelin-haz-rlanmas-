ed7fe730716ee364f8eaeac8c2468002757dddc0
qafarlicavid <javidig@code.edu.az>
2022-11-13 17:14:23 +0400

    Pages folder is ready

---
+++ Admin panelin hazırlanması/Admin panelin hazırlanması/Pages/Controllers/PricingController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Admin_panelin_hazırlanması.Pages.Controllers
+{
+    public class PricingController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ Admin panelin hazırlanması/Admin panelin hazırlanması/Pages/Controllers/ProfileController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Admin_panelin_hazırlanması.Pages.Controllers
+{
+    public class ProfileController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
